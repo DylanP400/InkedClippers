b45f739c6ebc86bc2dd8c0c84e861a1cb83a3ac3
Dylan <Dylanmcmahon187@gmail.com>
2023-06-25 19:55:47 +0000

    Revert "Initial Heroku deployment 2"
    This reverts commit 93266bbfde39fe7c075dd3ee50a502123413aa42.
    
    Fixed bad commits

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn inkedclippers.wsgi 
--- inkedclippers/settings.py
+++ inkedclippers/settings.py
@@ -18,7 +18,7 @@ if os.path.isfile('env.py'):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-dylanp400-inkedclippers-9j1n0bth96l.ws-eu100.gitpod.io', 'inked-clippers.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['8000-dylanp400-inkedclippers-9j1n0bth96l.ws-eu100.gitpod.io']
 
 
 # Application definition
@@ -40,9 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'cloudinary_storage',
     'django.contrib.staticfiles',
-    'cloudinary',
     'core',
 ]
 
@@ -61,7 +59,7 @@ ROOT_URLCONF = 'inkedclippers.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -128,13 +126,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-MEDIA_URL = '/media/'
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
