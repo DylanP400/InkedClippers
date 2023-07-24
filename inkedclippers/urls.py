3be481fb275089f4c4cceae17627f714574e69c4
Dylan <Dylanmcmahon187@gmail.com>
2023-07-24 21:30:20 +0000

    Heroku (error)

--- inkedclippers/urls.py
+++ inkedclippers/urls.py
@@ -18,6 +18,9 @@ from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from users import views as user_views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register-page'),
@@ -25,5 +28,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login-page'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout-page'),
     path('', include('core.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
