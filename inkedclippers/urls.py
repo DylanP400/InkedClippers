6c8de45de516497bd213b4a373862623faffcd39
Dylan <Dylanmcmahon187@gmail.com>
2023-11-14 00:09:33 +0000

    Fixed indentation error

--- inkedclippers/urls.py
+++ inkedclippers/urls.py
@@ -25,8 +25,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register-page'),
     path('profile/', user_views.profile, name='profile-page'),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login-page'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout-page'),
+    path('login/', auth_views.LoginView.as_view(
+        template_name='users/login.html'), name='login-page'),
+    path('logout/', auth_views.LogoutView.as_view(
+        template_name='users/logout.html'), name='logout-page'),
     path('', include('core.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
